f28669ebf30ff1f4c2dcf42da3bd2fbdeaf2d4cc
PSK6262 <qkrxotn60@gmail.com>
2026-06-22 12:15:32 +0900

    image 데이터 id 5까지 추가

--- GreenWay/Assets/components/Main.jsx
+++ GreenWay/Assets/components/Main.jsx
@@ -3,65 +3,54 @@ import { ListGroup , Form } from 'react-bootstrap';
 import "./Main.css";
 
 function Main({Data}){
-    const [showIntro, setShowIntro] = useState(true);
-    const [searchText, setSearchText] = useState("");
-    const [hidePark, setHidePark] = useState(false);
-    const [hideTrail, setHideTrail] = useState(false);
+    const [showIntro, setShowIntro] = useState(true); // 제일 처음 화면 보이기, 이후 false
+    const [searchText, setSearchText] = useState(""); // 검색용
+    const [hidePark, setHidePark] = useState(false); // 공원 숨기기
+    const [hideTrail, setHideTrail] = useState(false); // 산책로 숨기기
     const mapRef = useRef(null);
     const markerRef = useRef(null);
 
-    const filteredData = Data.filter((item) => {
+    const filteredData = Data.filter((item) => { // 오른쪽 Group에서 보여줄 것, true인것만 표현
         if (hidePark && item.type === "공원") return false;
         if (hideTrail && item.type === "산책로") return false;
-        if (searchText &&
-            !item.name.toLowerCase().includes(searchText.toLowerCase()))
-            { 
-                return false; 
-            }
-
+        if (searchText && !item.name.toLowerCase().includes(searchText.toLowerCase())){ 
+            return false; 
+        }
         return true;
     });
-
-    useEffect(() => {
+    useEffect(() => { // 네이버 지도 표시 (API 사용)
         if (!window.naver) return;
+        const mapOptions = {
+            center: new window.naver.maps.LatLng(
+                37.3595704,
+                127.105399
+            ),
+            zoom: 10
+        };
+            mapRef.current = new window.naver.maps.Map(
+            "map",
+            mapOptions
+        );
+    }, []); 
 
-    const mapOptions = {
-        center: new window.naver.maps.LatLng(
-            37.3595704,
-            127.105399
-        ),
-        zoom: 10
-    };
-        mapRef.current = new window.naver.maps.Map(
-        "map",
-        mapOptions
-    );
-    }, []);
-
-    const moveMap = (place) => {
-
+    const moveMap = (place) => { // Group에서 새로운 위치를 찍으면 이동한다.
         if (!mapRef.current) return;
-
         const location = new window.naver.maps.LatLng(
             place.lat,
             place.lng
         );
-
         mapRef.current.panTo(location);
         mapRef.current.setZoom(15);
-
-        // 기존 마커 제거
+        // 기존 마커 지우기
         if (markerRef.current) {
             markerRef.current.setMap(null);
         }
-
-        // 새 마커 생성
+        // 새 마커 만들기
         markerRef.current = new window.naver.maps.Marker({
             position: location,
             map: mapRef.current
         });
     };    
-
     return(
         <>
             <div className={'main-bg '+ (showIntro ? 'start' : 'fade-out')} 
@@ -97,7 +86,8 @@ function Main({Data}){
                     {/* 검색하면 아래 리스트에서 해당하는것만 나오게. */}
                     <ListGroup className="nameList">
                     {
-                        filteredData.length === 0
+                        // 만약 길이가 0 이다 -> 아무것도 없다.
+                        filteredData.length === 0 
                         ? (
                             <ListGroup.Item>
                                 조건에 맞는 결과가 없습니다.
@@ -122,7 +112,7 @@ function Main({Data}){
                             type="switch"
                             id="trail-switch"
                             label="산책로 숨김"
-                            className="me-4" /* 오른쪽 간격 띄우기 */
+                            className="me-5" /* 오른쪽 간격 띄우기 */
                             checked={hideTrail}
                             onChange={(e) => setHideTrail(e.target.checked)}
                         />
